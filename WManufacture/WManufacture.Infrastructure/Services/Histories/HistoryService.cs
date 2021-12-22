2a26302dc11283a1210bbb0b45469ac5b7540113
MIvashniou <maksim.ivashniou@gmail.com>
2021-12-23 01:18:02 +0300

    WManufacure.Infrastructure.Services: Create services. Add CRUD operations.
    Created History service. Added CRUD operations.

---
+++ WManufacture/WManufacture.Infrastructure/Services/Histories/HistoryService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using WManufacture.Common.Entity;
+using WManufacture.Infrastructure.Databases;
+
+namespace WManufacture.Infrastructure.Services.Histories
+{
+    public class HistoryService : IHistoryService
+    {
+        private readonly WManufactureContext _db;
+
+        private readonly ILogger<HistoryService> _logger;
+
+        public HistoryService(
+            WManufactureContext db,
+            ILogger<HistoryService> logger)
+        {
+            _db = db;
+
+            _logger = logger;
+        }
+
+        public async Task CreateAsync(History data)
+        {
+            if (data.Id == 0)
+            {
+                _db.Histories.Add(data);
+
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ WManufacture/WManufacture.Infrastructure/Services/Histories/IHistoryService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WManufacture.Common.Entity;
+
+namespace WManufacture.Infrastructure.Services.Histories
+{
+    public interface IHistoryService
+    {
+        Task CreateAsync(History data);
+    }
+}
